8f20a83a41966b935ce0f4040015823972231b65
Louisi de Mello Tomasi Dalazen <78367618+Louisii@users.noreply.github.com>
2023-04-23 13:06:42 -0300

    connection to db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'admin',
+    database: 'todolist',
+})
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database: ', err);
+      return;
+    }
+    console.log('Connected to database successfully!');
+  });
+
 
 app.get('/', (req, res) => {
-    res.send("hello")
+    const sqlInsert = "insert into task (taskname, taskdone) values ('buy flowers', 0);"
+    db.query(sqlInsert, (err, result) => {
+        res.send("hello lou")
+    })
+    
 })
 
 app.listen(3001, () => {
