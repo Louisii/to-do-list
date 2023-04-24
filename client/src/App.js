ce2f2118eeb801202f296aa39b8564d90f2f3ec1
Louisi de Mello Tomasi Dalazen <78367618+Louisii@users.noreply.github.com>
2023-04-24 20:12:45 -0300

    tasks array

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,10 @@ import logo from './logo.png';
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
 
 function App() {
 
-
-  const [checkboxStates, setCheckboxStates] = useState([false, false]);
+  const [checkboxStates, setCheckboxStates] = useState([true, false]);
 
   const handleCheckboxChange = (index) => {
     const newCheckboxStates = [...checkboxStates];
@@ -15,6 +13,18 @@ function App() {
     setCheckboxStates(newCheckboxStates);
   }
 
+  const tasks = [
+    {
+      name: "Prepare the monthly report",
+      done: checkboxStates[0],
+      onChange: () => handleCheckboxChange(0)
+    },
+    {
+      name: "Lunch with Zack",
+      done: checkboxStates[1],
+      onChange: () => handleCheckboxChange(1)
+    }
+  ];
 
   return (
     <div className='h-screen bg-gradient-to-r from-fuchsia-600 to-purple-600 md:px-16 lg:px-36 xl:px-52 2xl:px-96'>
@@ -22,17 +32,22 @@ function App() {
       <img src={logo} className="w-48 p-4 md:w-60" alt="" />
       <div className='bg-white rounded-md m-4 p-4 '>
         <div className='grid grid-cols-4'>
-          <input className="border-2 rounded-md p-2 col-span-3" placeholder='Task to be done...' type="text" name="newTask"/>
+          <input className="border-2 rounded-md p-2 col-span-3" placeholder='Task to be done...' type="text" name="newTask" />
           <button className='bg-purple-700 ml-4 p-2 rounded-md text-white font-semibold'>Add</button>
         </div>
       </div>
 
       <div className='bg-white rounded-md m-4 p-4 '>
 
-        <Task taskName="Prepare the monthly report" taskDone={checkboxStates[0]} onChange={() => handleCheckboxChange(0)}/>
-        <Task taskName="Lunch with Zack" taskDone={checkboxStates[1]} onChange={() => handleCheckboxChange(1)}/>
+        {tasks.map((task, index) => (
+          <Task
+            key={index}
+            taskName={task.name}
+            taskDone={task.done}
+            onChange={task.onChange}
+          />
+        ))}
 
-        
 
       </div>
 
