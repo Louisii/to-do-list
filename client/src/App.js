b5a7b231e142922994835a8c51a0208db50b9e9e
Louisi de Mello Tomasi Dalazen <78367618+Louisii@users.noreply.github.com>
2023-04-24 19:11:09 -0300

    task component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.png';
 import './App.css';
+import Task from './components/Task';
+import React, { useState } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 
 function App() {
+
+
+  const [checkboxStates, setCheckboxStates] = useState([false, false]);
+
+  const handleCheckboxChange = (index) => {
+    const newCheckboxStates = [...checkboxStates];
+    newCheckboxStates[index] = !newCheckboxStates[index];
+    setCheckboxStates(newCheckboxStates);
+  }
+
+
   return (
     <div className='h-screen bg-gradient-to-r from-fuchsia-600 to-purple-600 md:px-16 lg:px-36 xl:px-52 2xl:px-96'>
 
-
       <img src={logo} className="w-48 p-4 md:w-60" alt="" />
-
       <div className='bg-white rounded-md m-4 p-4 '>
         <div className='grid grid-cols-4'>
           <input className="border-2 rounded-md p-2 col-span-3" placeholder='Task to be done...' type="text" name="newTask"/>
@@ -18,21 +29,10 @@ function App() {
 
       <div className='bg-white rounded-md m-4 p-4 '>
 
-        <div className='grid grid-cols-7 mb-4 md:grid-cols-12'>
-          <input className='w-5' type="checkbox" />
-          <p className='col-span-5 md:col-span-10'>Prepare the monthly report</p>
-          <div className='text-end'>
-            <button className='bg-purple-700 p-2 rounded-md text-white font-semibold w-8'><FaTrashAlt /></button>
-          </div>
-        </div>
+        <Task taskName="Prepare the monthly report" taskDone={checkboxStates[0]} onChange={() => handleCheckboxChange(0)}/>
+        <Task taskName="Lunch with Zack" taskDone={checkboxStates[1]} onChange={() => handleCheckboxChange(1)}/>
 
-        <div className='grid grid-cols-7 mb-4 md:grid-cols-12'>
-          <input className='w-5' type="checkbox" />
-          <p className='col-span-5 md:col-span-10'>Lunch with Zack</p>
-          <div className='text-end'>
-            <button className='bg-purple-700 p-2 rounded-md text-white font-semibold w-8'><FaTrashAlt /></button>
-          </div>
-        </div>
+        
 
       </div>
 
---
+++ client/src/components/Task.jsx
@@ -0,0 +1,16 @@
+import { FaTrashAlt } from 'react-icons/fa';
+
+const Task = (props) => {
+   
+    return(
+        <div className='grid grid-cols-7 mb-4 md:grid-cols-12 border-b-2 pb-2'>
+          <input className='w-5' type="checkbox"  checked={props.taskDone} onChange={props.onChange}/>
+          <p className='col-span-5 md:col-span-10'>{props.taskName} </p>
+          <div className='text-end'>
+            <button className='bg-purple-700 p-2 rounded-md text-white font-semibold w-8'><FaTrashAlt /></button>
+          </div>
+        </div>
+    )
+}
+
+export default Task
