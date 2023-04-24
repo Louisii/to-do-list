039d0d814a3c4674f43c4f39c10f1f5a0e3e5b75
Louisi de Mello Tomasi Dalazen <78367618+Louisii@users.noreply.github.com>
2023-04-24 20:42:41 -0300

    db con

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
+require('dotenv').config();
 
 const db = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
     user: 'root',
     password: 'admin',
     database: 'todolist',
@@ -19,13 +19,16 @@ db.connect((err) => {
   });
 
 
-app.get('/', (req, res) => {
-    const sqlInsert = "insert into task (taskname, taskdone) values ('buy flowers', 0);"
-    db.query(sqlInsert, (err, result) => {
-        res.send("hello lou")
-    })
-    
-})
+  app.get('/', async (req, res) => {
+    try {
+      const sqlInsert = "insert into task (taskname, taskdone) values ('buy flowers', 0);";
+      const result = await db.query(sqlInsert);
+      res.send("hello lou");
+    } catch (err) {
+      console.error('Error inserting task: ', err);
+      res.status(500).send('Error inserting task: ' + err.message);
+    }
+  });
 
 app.listen(3001, () => {
     console.log("running on port 3001")
